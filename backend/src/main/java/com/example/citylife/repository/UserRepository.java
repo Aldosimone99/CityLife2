1f8dddd59af0d7aa38926fc724c777494ce90cc4
Aldosimone99 <aldosimonedirosa@gmail.com>
2025-03-08 17:40:54 +0100

    restared backend 2

---
+++ backend/src/main/java/com/example/citylife/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.citylife.repository;
+
+import com.example.citylife.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
