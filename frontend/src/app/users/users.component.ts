11a330cfc922bddb11d970d81d1e89ca0ca6f61d
Aldosimone99 <aldosimonedirosa@gmail.com>
2025-04-02 12:07:17 +0200

    fixed styles of users page

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -14,16 +14,6 @@ export class UsersComponent implements OnInit {
   users: any[] = [];
   filteredUsers: any[] = [];
   searchTerm: string = '';
-  newUser: any = {
-    name: '',
-    email: '',
-    gender: 'male',
-    status: 'active'
-  };
-  isAddUserFormVisible: boolean = false;
-  isDeleteConfirmationVisible: boolean = false;
-  userToDelete: any = null;
-  emailExistsError: boolean = false;
   usersPerPage: number = 10; // Default number of users per page
   usersPerPageOptions: number[] = [5, 10, 20, 50]; // Options for users per page
   loggedInUserId!: number;
@@ -61,76 +51,8 @@ export class UsersComponent implements OnInit {
     this.filteredUsers = filtered.slice(0, this.usersPerPage); // Update the displayed users based on the search term and selected number
   }
 
-  createUser() {
-    if (this.checkEmailExists(this.newUser.email)) {
-      this.emailExistsError = true;
-      return;
-    }
-    console.log('Creating user:', this.newUser); // Log per debug
-    this.userService.createUser(this.newUser).subscribe(user => {
-      this.users.push(user);
-      this.filteredUsers.push(user);
-      this.resetForm();
-      this.hideAddUserForm();
-      this.emailExistsError = false;
-    }, error => {
-      console.error('Error creating user:', error); // Log per debug
-    });
-  }
-
-  checkEmailExists(email: string): boolean {
-    return this.users.some(user => user.email === email);
-  }
-
-  deleteUser(id: number) {
-    this.userToDelete = this.users.find(user => user.id === id);
-    this.isDeleteConfirmationVisible = true;
-  }
-
-  confirmDeleteUser() {
-    if (this.userToDelete) {
-      this.http.delete(`/api/users/${this.userToDelete.id}`).subscribe(
-        () => {
-          this.users = this.users.filter(user => user.id !== this.userToDelete.id);
-          this.filteredUsers = this.filteredUsers.filter(user => user.id !== this.userToDelete.id);
-          this.isDeleteConfirmationVisible = false;
-          this.userToDelete = null;
-        },
-        (error) => {
-          console.error('Error deleting user:', error);
-        }
-      );
-    }
-  }
-
-  cancelDeleteUser() {
-    this.isDeleteConfirmationVisible = false;
-    this.userToDelete = null;
-  }
-
   viewUser(id: number) {
     this.router.navigate(['/user', id]);
-  }
-
-  onSubmit() {
-    this.createUser();
-  }
-
-  resetForm() {
-    this.newUser = {
-      name: '',
-      email: '',
-      gender: 'male',
-      status: 'active'
-    };
-  }
-
-  showAddUserForm() {
-    this.isAddUserFormVisible = true;
-  }
-
-  hideAddUserForm() {
-    this.isAddUserFormVisible = false;
   }
 
   updateUsersPerPage() {
