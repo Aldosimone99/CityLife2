d6510fef7018fcc0e73e41f6903f2af4e30f0bc9
Aldosimone99 <aldosimonedirosa@gmail.com>
2025-04-02 12:39:38 +0200

    added profile img nel profile pag

--- frontend/src/app/animations.ts
+++ frontend/src/app/animations.ts
@@ -3,6 +3,6 @@ import { trigger, transition, style, animate } from '@angular/animations';
 export const fadeAnimation = trigger('fadeAnimation', [
   transition('* <=> *', [ // Applica l'animazione a tutte le transizioni di pagina
     style({ opacity: 0 }),
-    animate('400ms ease-in-out', style({ opacity: 1 }))
+    animate('800ms ease-in-out', style({ opacity: 1 }))
   ])
 ]);
